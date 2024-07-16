1bcbd5933c3dd7143f83c9cb6078f55f6b0640a2
soli <solidaryl@outlook.com>
2024-07-16 20:19:08 +0800

    修改client为非阻塞IO，添加服务停止函数

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,11 @@ const Allocator = std.mem.Allocator;
 const Client = struct {
     c: std.net.Server.Connection,
     ev: linux.epoll_event,
-    buffer: [256]u8,
+    rbuf: [2048]u8,
+    wbuf: [2048]u8,
+    wlen: i32,
+
+    pub fn write(_: []u8) void {}
 };
 const host = "127.0.0.1";
 const port = 8080;
@@ -19,16 +23,22 @@ const Server = struct {
 
     allocator: Allocator,
     clients: std.AutoHashMap(i32, Client),
+    running: bool,
 
     pub fn init(allocator: Allocator) !Server {
         return Server{
             .allocator = allocator,
             .clients = std.AutoHashMap(i32, Client).init(allocator),
+            .running = false,
         };
     }
 
     pub fn deinit(self: *Self) void {
         std.log.info("server deinit: {d}", .{max_size});
+        var it = self.*.clients.iterator();
+        while (it.next()) |kv| {
+            kv.value_ptr.c.stream.close();
+        }
         self.*.clients.deinit();
     }
 
@@ -36,6 +46,9 @@ const Server = struct {
         log.info("server event {d}", .{e.data.fd});
         if (e.events == linux.EPOLL.IN) {
             const connect = try server.accept();
+            errdefer connect.stream.close();
+            // 设置socket为非阻塞
+            _ = try posix.fcntl(connect.stream.handle, posix.F.SETFL, posix.SOCK.NONBLOCK);
 
             const key = connect.stream.handle;
             try self.*.clients.put(key, .{
@@ -44,12 +57,11 @@ const Server = struct {
                     .events = linux.EPOLL.IN | linux.EPOLL.ET,
                     .data = linux.epoll_data{ .fd = key },
                 },
-                .buffer = undefined,
+                .rbuf = undefined,
+                .wbuf = undefined,
+                .wlen = 0,
             });
-            errdefer {
-                _ = self.*.clients.remove(key);
-                connect.stream.close();
-            }
+            errdefer _ = self.*.clients.remove(key);
 
             const client = self.*.clients.getPtr(connect.stream.handle);
 
@@ -61,39 +73,40 @@ const Server = struct {
     }
 
     fn handle_client_event(self: *Self, _: i32, e: *const linux.epoll_event) !void {
-        log.info("client event {d}", .{e.data.fd});
-
-        if (e.events & linux.EPOLL.IN > 0) {
-            log.info("client read event: {d}", .{e.data.fd});
-            while (true) {
-                var buf: [16]u8 = undefined;
-                const n = posix.read(e.data.fd, &buf) catch |err| {
-                    log.err("failed to read data: {?}", .{err});
-                    _ = self.*.clients.remove(e.data.fd);
-                    break;
-                };
-                if (n == 0) {
-                    log.info("read data len is 0", .{});
-                    break;
+        if (self.clients.getPtr(e.data.fd)) |client| {
+            if (e.events & linux.EPOLL.IN > 0) {
+                log.info("client read event: {d}", .{e.data.fd});
+                while (true) {
+                    if (client.c.stream.read(&client.rbuf)) |n| {
+                        if (n == 0) {
+                            log.info("read data len is 0", .{});
+                            defer {
+                                client.c.stream.close();
+                                _ = self.clients.remove(e.data.fd);
+                            }
+                            break;
+                        }
+                        log.info("read data: {s}", .{client.rbuf[0..n]});
+                    } else |err| switch (err) {
+                        error.WouldBlock => break,
+                        else => {
+                            log.err("failed to read data: {?}", .{err});
+                            break;
+                        },
+                    }
                 }
-                log.info("read data: {s}", .{buf[0..n]});
             }
-        }
 
-        if (e.events & linux.EPOLL.OUT > 0) {
-            log.info("client write event: {d}", .{e.data.fd});
-            const ret = "hello world!\n";
-            // const l = posix.write(e.data.fd, ret) catch |err| {
-            const client = self.*.clients.get(e.data.fd) orelse {
-                log.warn("get client is empty", .{});
-                return;
-            };
-            const l = client.c.stream.write(ret) catch |err| {
-                log.err("failed to write data: {?}", .{err});
-                _ = self.*.clients.remove(e.data.fd);
-                return;
-            };
-            log.info("write data success, len: {d}", .{l});
+            if (e.events & linux.EPOLL.OUT > 0) {
+                log.info("client write event: {d}", .{e.data.fd});
+                const l = client.c.stream.write(client.wbuf[0..@intCast(client.wlen)]) catch |err| {
+                    log.err("failed to write data: {?}", .{err});
+                    _ = self.*.clients.remove(e.data.fd);
+                    return;
+                };
+                // todo: 写事件处理完后需要操作epoll_ctl删除监听
+                log.info("write data success, len: {d}", .{l});
+            }
         }
     }
 
@@ -130,7 +143,8 @@ const Server = struct {
         var events: [max_size]linux.epoll_event = undefined;
         var events_count: usize = 0;
 
-        while (true) {
+        self.running = true;
+        while (self.running) {
             events_count = posix.epoll_wait(epfd, events[0..max_size], -1);
             for (events[0..events_count]) |e| {
                 if (e.data.fd == server.stream.handle) { // 服务端监听服务fd
@@ -145,7 +159,17 @@ const Server = struct {
             }
         }
     }
+
+    pub fn stop_server(self: *Self) void {
+        self.running = false;
+    }
 };
+
+fn stop_server(server: *Server) void {
+    log.info("run stop server thread", .{});
+    std.time.sleep(std.time.ns_per_min * 30);
+    server.stop_server();
+}
 
 pub fn main() void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -160,6 +184,10 @@ pub fn main() void {
         return;
     };
     defer server.deinit();
+
+    _ = std.Thread.spawn(.{}, stop_server, .{&server}) catch |err| {
+        log.err("failed to run stop server thread. {?}", .{err});
+    };
 
     server.run_server();
 }
